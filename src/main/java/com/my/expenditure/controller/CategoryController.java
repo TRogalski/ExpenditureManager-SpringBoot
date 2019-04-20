0a17fa481189984ef40a58ce51d034c06559981a
TRogalski <tomracc@gmail.com>
2019-04-20 23:05:28 +0200

    Category/subcategory addition test

--- src/main/java/com/my/expenditure/controller/CategoryController.java
+++ src/main/java/com/my/expenditure/controller/CategoryController.java
@@ -1,11 +1,12 @@
 package com.my.expenditure.controller;
 
 import com.my.expenditure.entity.Category;
+import com.my.expenditure.entity.Subcategory;
 import com.my.expenditure.entity.User;
 import com.my.expenditure.repository.CategoryRepository;
+import com.my.expenditure.repository.SubcategoryRepository;
 import com.my.expenditure.service.MyUserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/category")
@@ -22,6 +23,9 @@ public class CategoryController {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private SubcategoryRepository subcategoryRepository;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     private String getAddCategoryView(Model model) {
@@ -36,7 +40,50 @@ public class CategoryController {
         User user = myUserPrincipal.getUser();
         category.setUser(user);
         categoryRepository.save(category);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    private String list() {
         return "category/categoryListTest";
+    }
+
+    @RequestMapping(value = "/addsub", method = RequestMethod.GET)
+    private String getAddSubcategoryView(Model model) {
+        model.addAttribute("subcategory", new Subcategory());
+        return "subcategory/addSubcategoryTest";
+    }
+
+    @RequestMapping(value = "/addsub", method = RequestMethod.POST)
+    private String addSubcategory(@ModelAttribute Subcategory subcategory) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MyUserPrincipal myUserPrincipal = (MyUserPrincipal) authentication.getPrincipal();
+        User user = myUserPrincipal.getUser();
+        subcategory.setUser(user);
+        subcategoryRepository.save(subcategory);
+        return "redirect:sublist";
+    }
+
+    @RequestMapping(value = "/sublist", method = RequestMethod.GET)
+    private String subList() {
+        return "subcategory/subcategoryListTest";
+
+    }
+
+    @ModelAttribute("categories")
+    public List<Category> categories() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MyUserPrincipal myUserPrincipal = (MyUserPrincipal) authentication.getPrincipal();
+        User user = myUserPrincipal.getUser();
+        return categoryRepository.findAllByUser(user);
+    }
+
+    @ModelAttribute("subcategories")
+    public List<Subcategory> subcategories() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MyUserPrincipal myUserPrincipal = (MyUserPrincipal) authentication.getPrincipal();
+        User user = myUserPrincipal.getUser();
+        return subcategoryRepository.findAllByUser(user);
     }
 
 }
--- src/main/java/com/my/expenditure/repository/CategoryRepository.java
+++ src/main/java/com/my/expenditure/repository/CategoryRepository.java
@@ -1,9 +1,14 @@
 package com.my.expenditure.repository;
 
 import com.my.expenditure.entity.Category;
+import com.my.expenditure.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findAllByUser(User user);
 }
