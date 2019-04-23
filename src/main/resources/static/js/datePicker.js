816fb14156ce381075beb538d56af532fb484908
TRogalski <tomracc@gmail.com>
2019-04-23 21:57:50 +0200

    working on JS to dynamically update list of expenditures based on datepicker selection

--- src/main/resources/static/js/datePicker.js
+++ src/main/resources/static/js/datePicker.js
@@ -1,13 +1,53 @@
-//change hidden value on click
+document.addEventListener('DOMContentLoaded', function () {
 
-$('#date_picker').on('changeDate', function () {
-    $('#date_picker_input').val(
-        $('#date_picker').datepicker('getFormattedDate')
-    );
+    $('#date_picker').datepicker({
+        format: 'yyyy-mm-dd',
+    }).datepicker("setDate", 'now');
 
-});
 
-$('#date_picker').datepicker({
-    format: 'yyyy-mm-dd',
-}).datepicker("setDate", 'now');
+    $('#date_picker').on('changeDate', function () {
 
+        //change hidden value on click  - not needed
+        // $('#date_picker_input').val(
+        //     $('#date_picker').datepicker('getFormattedDate')
+        // );
+
+        getExpendituresAssignedToDate($('#date_picker').datepicker('getFormattedDate'));
+
+        console.log("zmieniono date!");
+    });
+
+
+})
+
+
+// var expenditure_records = document.getElementById("expenditure_records");
+//
+// expenditure_records.addEventListener('click', function () {
+//     console.log("kliknieto mnie!")
+// })
+
+
+function getExpendituresAssignedToDate(date) {
+    fetch("http://localhost:8084/expenditure/date/" + date).then(function (response) {
+        return response.json();
+    }).then(function (dateExpendituresJson) {
+        console.log(JSON.stringify(dateExpendituresJson));
+        appendReceivedElements(dateExpendituresJson)
+    });
+
+    removeEnlistedElements()
+}
+
+function removeEnlistedElements() {
+    var toDelete = document.getElementById("expenditure_records");
+    while (toDelete.hasChildNodes()) {
+        toDelete.removeChild(toDelete.lastChild);
+    }
+}
+
+function appendReceivedElements(dateExpendituresJson){
+    for(var i=0;i<dateExpendituresJson.length;i++){
+        console.log(dateExpendituresJson[i])
+    }
+}
