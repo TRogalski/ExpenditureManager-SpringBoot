f8826031363251abbc3b2ff8e5ce0db842422caf
TRogalski <tomracc@gmail.com>
2019-05-17 21:13:52 +0200

    heroku test

--- src/main/java/com/my/expenditure/configuration/DatabaseConfig.java
+++ src/main/java/com/my/expenditure/configuration/DatabaseConfig.java
@@ -3,6 +3,7 @@ package com.my.expenditure.configuration;
 import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+
 import javax.sql.DataSource;
 
 @Configuration
--- src/main/java/com/my/expenditure/repository/ExpenditureRepository.java
+++ src/main/java/com/my/expenditure/repository/ExpenditureRepository.java
@@ -22,7 +22,7 @@ public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> 
 //    @Query("SELECT e FROM Expenditure e where SUBSTRING(e.date,6,2)=SUBSTRING(:date,6,2) and SUBSTRING(e.date,1,4)=SUBSTRING(:date,1,4) and e.user=:user")
 //    List<Expenditure> findAllByUserAndMonth(@Param("user") User user,@Param("date") String date);
 
-    @Query("SELECT e FROM Expenditure e where MONTH(e.date)=MONTH(:date) and YEAR(e.date)=YEAR(:date) and e.user=:user")
+    @Query("SELECT e FROM Expenditure e where EXTRACT(MONTH FROM e.date)=EXTRACT(MONTH FROM :date) and EXTRACT(YEAR FROM e.date)=EXTRACT( YEAR FROM :date) and e.user=:user")
     List<Expenditure> findAllByUserAndMonth(@Param("user") User user, @Param("date") Date date);
 
 //    @Query("SELECT SUM(e.amount) FROM Expenditure e where " +
@@ -32,28 +32,28 @@ public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> 
 //    Double getCurrentMonthTotal(@Param("user") User user, @Param("date") String date);
 
     @Query("SELECT SUM(e.amount) FROM Expenditure e where " +
-            "YEAR(e.date)=YEAR(:date) and " +
-            "MONTH(e.date)=MONTH(:date) and " +
+            "EXTRACT(YEAR FROM e.date)=EXTRACT(YEAR FROM :date) and " +
+            "EXTRACT(MONTH FROM e.date)=EXTRACT(MONTH FROM :date) and " +
             "e.user=:user")
     Double getCurrentMonthTotal(@Param("user") User user, @Param("date") Date date);
 
 //    @Query("SELECT SUM(e.amount) FROM Expenditure  e where SUBSTRING(e.date,1,4)=SUBSTRING(:date,1,4) and e.user=:user")
 //    Double getCurrentYearTotal(@Param("user") User user, @Param("date") String date);
 
-    @Query("SELECT SUM(e.amount) FROM Expenditure  e where YEAR(e.date)=YEAR(:date) and e.user=:user")
+    @Query("SELECT SUM(e.amount) FROM Expenditure  e where EXTRACT(YEAR FROM e.date)=EXTRACT(YEAR FROM :date) and e.user=:user")
     Double getCurrentYearTotal(@Param("user") User user, @Param("date") Date date);
 
 //    @Query("SELECT SUM(e.amount) FROM Expenditure  e where SUBSTRING(e.date,1,4)=(SUBSTRING(:date,1,4)) and e.user=:user")
 //        //-1
 //    Double getPreviousYearTotal(@Param("user") User user, @Param("date") String date);
 
-    @Query("SELECT SUM(e.amount) FROM Expenditure  e where YEAR(e.date,1,4)=(YEAR(:date,1,4)-1) and e.user=:user")
+    @Query("SELECT SUM(e.amount) FROM Expenditure  e where EXTRACT(YEAR FROM e.date)=(EXTRACT(YEAR FROM :date)-1) and e.user=:user")
     Double getPreviousYearTotal(@Param("user") User user, @Param("date") Date date);
 
 //    @Query("SELECT e FROM Expenditure e WHERE SUBSTRING(e.date,1,4)=SUBSTRING(:date,1,4) and e.user=:user")
 //    List<Expenditure> getCurrentYearMonthlyExpenditures(@Param("user") User user, @Param("date") String date);
 
-    @Query("SELECT e FROM Expenditure e WHERE YEAR(e.date)=YEAR(:date) and e.user=:user")
+    @Query("SELECT e FROM Expenditure e WHERE EXTRACT(YEAR FROM e.date)=EXTRACT(YEAR FROM :date) and e.user=:user")
     List<Expenditure> getCurrentYearMonthlyExpenditures(@Param("user") User user, @Param("date") Date date);
 
 //    @Query("SELECT e FROM Expenditure e " +
@@ -65,8 +65,8 @@ public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> 
 
     @Query("SELECT e FROM Expenditure e " +
             "WHERE :tag MEMBER OF e.tags " +
-            "AND MONTH(e.date)=MONTH(:date) " +
-            "AND YEAR(e.date)=YEAR(:date) " +
+            "AND EXTRACT(MONTH FROM e.date)=EXTRACT(MONTH FROM :date) " +
+            "AND EXTRACT(YEAR FROM e.date)=EXTRACT(YEAR FROM :date) " +
             "AND e.user=:user")
     List<Expenditure> findAllByTagAndDateAndUser(@Param("tag") Tag tag, @Param("date") Date date, @Param("user") User user);
 
@@ -80,8 +80,8 @@ public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> 
 
     @Query("SELECT e FROM Expenditure e " +
             "WHERE e.tags is empty " +
-            "AND MONTH(e.date)=MONTH(:date) " +
-            "and YEAR(e.date)=YEAR(:date) " +
+            "AND EXTRACT(MONTH FROM e.date)=EXTRACT(MONTH FROM :date) " +
+            "and EXTRACT(YEAR FROM e.date)=EXTRACT(YEAR FROM :date) " +
             "and e.user=:user")
     List<Expenditure> findAllUnassignedByDateAndUser(@Param("date") Date date, @Param("user") User user);
 
@@ -93,8 +93,8 @@ public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> 
 //    Double getPreviousMonthTotal(@Param("user") User user, @Param("date") String date);
 
     @Query("SELECT SUM(e.amount) FROM Expenditure e where " +
-            "YEAR(e.date)=YEAR(:date) and " +
-            "MONTH(e.date)=(MONTH(:date)-1) and " +    //-1
+            "EXTRACT(YEAR FROM e.date)=EXTRACT(YEAR FROM :date) and " +
+            "EXTRACT(MONTH FROM e.date)=(EXTRACT(MONTH FROM :date)-1) and " +    //-1
             "e.user=:user")
     Double getPreviousMonthTotal(@Param("user") User user, @Param("date") Date date);
 
@@ -107,8 +107,8 @@ public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> 
 
     @Query("SELECT e FROM Expenditure e " +
             "WHERE :tag MEMBER OF e.tags " +
-            "AND MONTH(e.date)=(MONTH(:date)-1) " +  //-1
-            "AND YEAR(e.date)=YEAR(:date) " +
+            "AND EXTRACT(MONTH FROM e.date)=(EXTRACT(MONTH FROM :date)-1) " +  //-1
+            "AND EXTRACT(YEAR FROM e.date)=EXTRACT(YEAR FROM :date) " +
             "AND e.user=:user")
     List<Expenditure> findAllByTagAndDatePreviousAndUser(@Param("tag") Tag tag, @Param("date") Date date, @Param("user") User user);
 }
