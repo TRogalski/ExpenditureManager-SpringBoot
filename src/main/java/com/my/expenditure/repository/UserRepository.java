9e5341ce928ff92468f623a20e719a336e998fde
Tomasz Rogalski <tomracc@gmail.com>
2019-05-17 12:54:17 +0200

    heroku setup continued

--- src/main/java/com/my/expenditure/repository/UserRepository.java
+++ src/main/java/com/my/expenditure/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.my.expenditure.repository;
 
 import com.my.expenditure.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findFirstByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.email=:email")
+    User findFirstByEmail(@Param("email") String email);
 }
